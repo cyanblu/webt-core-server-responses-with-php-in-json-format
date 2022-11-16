d051599b4460d5477d4c8e1567d2ce72513235de
Moritz Liedl <9186@htl.rennweg.at>
2022-11-16 12:24:57 +0100

    index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+header('Content-Type: application/json; charset=utf-8');
+
+class Soundtrack implements JsonSerializable{
+    public string $name;
+    public string $band;
+
+    function __construct(string $name, string $band){
+        $this->name = $name;
+        $this->band = $band;
+    }
+
+    public function jsonSerialize(){
+        return array(
+            'name' => $this->name,
+            'band' => $this->band,
+            'keks' => 'ein keks'
+        )
+    }
+}
