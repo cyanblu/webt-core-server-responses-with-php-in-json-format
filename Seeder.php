419d55b34e6156d1c4c1b370d1c8267faaf5d6da
Christian Widauer <9198@htl.rennweg.at>
2022-11-23 11:12:44 +0100

    ids richtig gemacht

--- Seeder.php
+++ Seeder.php
@@ -10,13 +10,13 @@ class Seeder
                 new Song(2, "Moralboarer", "Christian Cyprey", "2", "1:55"),
                 new Song(3, "GigiMega", "Cyprian Chrissy", "3", "3:33")
             )),
-            new OSTs("1", "SecondOST", "Dark Mouth", 2007, array(
+            new OSTs(1, "SecondOST", "Dark Mouth", 2007, array(
                 new Song(1, "Chromesday", "Christian Wilder", "1", "3:24"),
                 new Song(0, "Firedome", "Cyprian Mark", "0", "2:15"),
                 new Song(3, "GigiMega", "Cyprian Chrissy", "3", "3:33"),
                 new Song(2, "Moralboarer", "Christian Cyprey", "2", "1:55")
             )),
-            new OSTs("2", "ThirdOST", "Light Mouth", 2015, array(
+            new OSTs(2, "ThirdOST", "Light Mouth", 2015, array(
                 new Song(2, "Moralboarer", "Christian Cyprey", "2", "1:55"),
                 new Song(3, "GigiMega", "Cyprian Chrissy", "3", "3:33"),
                 new Song(1, "Chromesday", "Christian Wilder", "1", "3:24"),
